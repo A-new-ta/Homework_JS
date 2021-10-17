5beb6aa36f46d414442ed7c528c74981a2804726
A-new-ta <urievna86@gmail.com>
2021-10-17 13:48:25 +0300

    feat: complete task

--- chess/script.js
+++ chess/script.js
@@ -1,24 +1,80 @@
 "use strict";
 
-// рисуем шахматную доску
+// доска с вариантами расстановки ферзей
 
-  let chessWrap = document.querySelector('.ch-wrap');
-  let i = 0, count = 0;
-  while (count < 8 * 8) {
-    let item = document.createElement('div');
-    chessWrap.appendChild(item);
-    item.classList.add('ch-item');
-    if (i && i % 2)
-      item.classList.add('ch-black')
-    i += ((i + 2) % 9) ? 1 : 2;
-    count++;
-  }
+function chessDesk(solution) {
+
+    let solutionArr = solution.split(',');
+        
+        let div = document.getElementsByClassName('chess-board')[0];
+        let table = document.createElement('table');
+        table.classList.add('chess-desk');
+
+        for (let i = 0; i < 8; i++) {
+            let tr = document.createElement('tr');
+            tr.classList.add('row');
+            for (let j = 0; j < 8; j++) {
+                let td = document.createElement('td');
+                if (i % 2 !== j % 2) {
+                    td.classList.add('black');
+                }
+                if (+(solutionArr[i]) === j) {
+                    td.innerHTML = '&#9813;';
+                    td.setAttribute('row-position', `${i}`);
+                    td.setAttribute('col-position', `${j}`);
+                    td.classList.add('queen');
+                    td.setAttribute('title',
+                    `нажмите кнопку мыши, чтобы посмотреть ходы
+нажмите кпонку мыши еще раз, чтобы скрыть ходы`);
+                    td.addEventListener('mousedown', mousedownChangeColor);
+                    // td.addEventListener('mouseup', mouseupChangeColor);
+                }
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+        }
+        div.appendChild(table);
 
 
+}
+
+
+// выбор варианта расстановки 
+function selectOption() {
+    let select = document.getElementById('select-solution');
+    let arr = searchPosition(8);
+    let disabledOption = document.createElement('option');
+    disabledOption.selected = true;
+    disabledOption.innerText = 'выберите вариант расстановки ферзей';
+    select.appendChild(disabledOption);
+
+    for (let k = 0; k < arr.length; k++) {
+        let item = document.createElement('option');
+        item.innerHTML = arr[k];
+        select.appendChild(item);
+    }
+}
+selectOption();
+
+
+// построение доски по выбранному варианту
+let select = document.getElementById('select-solution');
+select.addEventListener('change', changeSolution);
+
+function changeSolution(EO) {
+    // Удаляем старую доску
+    let table = document.getElementsByClassName('chess-board')[0];
+    table.innerHTML = '';
+
+    let index = EO.target.options.selectedIndex;
+    let solution = EO.target.options[index].innerText;
+    chessDesk(solution);
+}
+
+
+  
 // поиск возможных вариантов расстановки
-
 function searchPosition(boardNumber) {
-
     let currentSolution = [];
     let finalSolution = [];
     
@@ -36,12 +92,6 @@ function searchPosition(boardNumber) {
                         (prev + (length - row) === col)) {
                     break;
                     }
-                // if (prev - (length - row) === col) {
-                // break;
-                // }
-                // if (prev + (length - row) === col) {
-                // break;
-                // }
                 }
             if (row === length) {
                 search(currentSolution.concat([col]));
@@ -50,34 +100,33 @@ function searchPosition(boardNumber) {
         }
       
     };
-
   search([]);
-
   return finalSolution;
 };
 // console.log(searchPosition(8));
 
 
-// выбор варианта расстановки 
-function selectOption() {
-    let select = document.getElementById('select-solution');
-    let arr = searchPosition(8);
-    let disabledItem = document.createElement('option');
-    disabledItem.disabled = true;
-    disabledItem.selected = true;
-    disabledItem.innerText = 'выберите вариант расстановки';
-    select.appendChild(disabledItem);
+// подсвечивание клеток 
+function mousedownChangeColor(EO) {
+    let row = +(EO.target.getAttribute('row-position'));
+    let col = +(EO.target.getAttribute('col-position'));
+    let tr = document.getElementsByClassName('row');
+    let left = col - row; 
+    let right = col + row; 
 
-    for (let k = 0; k < arr.length; k++) {
-        let item = document.createElement('option');
-        item.text = arr[k];
-        item.value = arr[k];
-        select.appendChild(item.cloneNode(true));
+    for (let i = 0; i < 8; i++) {
+        let td = tr[i].childNodes;
+    
+        for (let k = 0; k < 8; k++) {
+            if (row === i && k === col) {
+                continue;
+            }
+            if ((k === left + i) || (k === right - i) || (i === row) || (k === col)) {
+                td[k].classList.toggle('moves');
+            }
+        }
     }
 }
-selectOption();
-
-
 
 
 
