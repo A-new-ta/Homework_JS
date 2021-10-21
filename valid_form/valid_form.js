55fd520c6794a4774e77f71a469cc540e3421d44
A-new-ta <urievna86@gmail.com>
2021-10-22 02:41:23 +0300

    feat:complete task

--- valid_form/valid_form.js
+++ valid_form/valid_form.js
@@ -12,46 +12,33 @@ let payment = form.elements.payment;
 let votes = document.querySelector("#votes");
 let description = document.querySelector("#description");
 
-developers.addEventListener('blur', EO => {
-    validDevelopers(0);
-}, false);
+let inputs = document.querySelectorAll('input');
 
-sitename.addEventListener('blur', EO => {
-    validSite(0);
-}, false);
 
-urlField.addEventListener('blur', EO => {
-    validUrl(0);
-}, false);
+developers.addEventListener('blur', validDevelopers, false);
 
-dateField.addEventListener('blur', EO => {
-    validDate(0);
-}, false);
 
-visitorsField.addEventListener('blur', EO => {
-    validVisitors(0);
-}, false);
+sitename.addEventListener('blur', validSite, false);
 
-emailField.addEventListener('blur', EO => {
-    validEmail(0);
-}, false);
+urlField.addEventListener('blur', validUrl, false);
 
-division.addEventListener('change', EO => {
-    validDivision(0);
-}, false);
+dateField.addEventListener('blur', validDate, false);
+
+visitorsField.addEventListener('blur', validVisitors, false);
+
+emailField.addEventListener('blur', validEmail, false);
+
+division.addEventListener('change', validDivision, false);
 
 for (let i = 0; i < payment.length; i++) {
     payment[i].addEventListener('change',validPayment,false);
 };
 
-votes.addEventListener('change', EO => {
-    validVotes(0);
-}, false);
+votes.addEventListener('change', validVotes, false);
 
-description.addEventListener('blur', EO => {
-    validDescription(0);
-}, false);
+description.addEventListener('blur', validDescription, false);
 
+form.addEventListener('submit', validFormAll, false);
 
 
 
@@ -61,15 +48,22 @@ function validDevelopers(EO) {
     let developersValue = developers.value;
     let developersErr = developers.nextElementSibling;
     if (!developersValue) {
-        developersErr.innerHTML = 'Введите разработчиков';
-        developers.setAttribute('style', 'background-color: #f5bcbc');
+        developersErr.innerHTML = 'Вы не ввели разработчиков';
+        developers.classList.add('errcolor');
+        // developers.focus();
+        return false;
+
     } else if (developersValue.length < 3 || developersValue.length > 30) {
         developersErr.innerHTML = 'Длина строки может быть от 3 до 30 символов';
-        developers.setAttribute('style', 'background-color: #f5bcbc');
+        developers.classList.add('errcolor');
+        return false;
+        
     } else {
         developersErr.innerHTML = '';
-        developers.setAttribute('style', 'background-color:none');
-    };
+        developers.classList.remove('errcolor');
+        return true;
+    }
+    
 }
 
 function validSite(EO) {
@@ -79,13 +73,17 @@ function validSite(EO) {
     let siteErr = sitename.nextElementSibling;
     if (!siteValue) {
         siteErr.innerHTML = 'Вы не ввели название сайта';
-        sitename.setAttribute('style', 'background-color: #f5bcbc');
+        sitename.classList.add('errcolor');
+        return false;
     } else if (siteValue.length > 30) {
-        siteErr.innerHTML = 'Название не может быть длиннее 20 символов';
-        sitename.setAttribute('style', 'background-color: #f5bcbc');
+        siteErr.innerHTML = 'Название не может быть длиннее 30 символов';
+        sitename.classList.add('errcolor');
+        sitename.focus();
+        return false;
     } else {
         siteErr.innerHTML = '';
-        sitename.setAttribute('style', 'background-color:none');
+        sitename.classList.remove('errcolor');
+        return true;
     };
 }
 
@@ -98,13 +96,16 @@ function validUrl(EO) {
     let pattern = /^(ftp|http|https):\/\/[^ "]+$/;
     if (!urlValue) {
         urlErr.innerHTML = 'Вы не ввели URL сайта';
-        urlField.setAttribute('style', 'background-color: #f5bcbc');
+        urlField.classList.add('errcolor');
+        return false;
     } else if (!pattern.test(urlValue)) {
-        urlErr.innerHTML = 'Введите корректный URL сайта';
-        urlField.setAttribute('style', 'background-color: #f5bcbc');
+        urlErr.innerHTML = 'Введите корректный URL сайта (должен начинаться с https://)';
+        urlField.classList.add('errcolor');
+        return false;
     } else {
         urlErr.innerHTML = '';
-        urlField.setAttribute('style', 'background-color:none');
+        urlField.classList.remove('errcolor');
+        return true;
     };
 }
 
@@ -118,16 +119,20 @@ function validDate(EO) {
     let dateErr = dateField.nextElementSibling;
     if (!dateValue) {
         dateErr.innerHTML = 'Вы не указали дату запуска сайта';
-        dateField.setAttribute('style', 'background-color: #f5bcbc');
+        dateField.classList.add('errcolor');
+        return false;
     } else if (dateValue > today) {
         dateErr.innerHTML = 'Дата запуска не может быть больше текущей даты ';
-        dateField.setAttribute('style', 'background-color: #f5bcbc');
+        dateField.classList.add('errcolor');
+        return false;
     } else if (dateValue < 1970) {
         dateErr.innerHTML = 'В те времена еще не было интернета ';
-        dateField.setAttribute('style', 'background-color: #f5bcbc');
+        dateField.classList.add('errcolor');
+        return false;
     } else {
         dateErr.innerHTML = '';
-        dateField.setAttribute('style', 'background-color:none');
+        dateField.classList.remove('errcolor');
+        return true;
     };
 }
 
@@ -139,16 +144,20 @@ function validVisitors(EO) {
     let visitorsErr = visitorsField.nextElementSibling;
     if (!visitorsValue) {
         visitorsErr.innerHTML = 'Вы не ввели количество посетителей';
-        visitorsField.setAttribute('style', 'background-color: #f5bcbc');
+        visitorsField.classList.add('errcolor');
+        return false;
     } else if (visitorsValue < 0) {
         visitorsErr.innerHTML = 'Количество посетителей не может быть отрицательным';
-        visitorsField.setAttribute('style', 'background-color: #f5bcbc');
+        visitorsField.classList.add('errcolor');
+        return false;
     } else if (!Number.isInteger(+(visitorsValue))) {
         visitorsErr.innerHTML = 'Введите целое число';
-        visitorsField.setAttribute('style', 'background-color: #f5bcbc');
+        visitorsField.classList.add('errcolor');
+        return false;
     } else {
         visitorsErr.innerHTML = '';
-        visitorsField.setAttribute('style', 'background-color:none');
+        visitorsField.classList.remove('errcolor');
+        return true;
     };
 }
 
@@ -160,13 +169,16 @@ function validEmail(EO) {
     let pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!emailValue) {
         emailErr.innerHTML = 'Вы не ввели email';
-        emailField.setAttribute('style', 'background-color: #f5bcbc');
+        emailField.classList.add('errcolor');
+        return false;
     } else if (!pattern.test(emailValue)) {
         emailErr.innerHTML = 'Введите корректный email';
-        emailField.setAttribute('style', 'background-color: #f5bcbc');
+        emailField.classList.add('errcolor');
+        return false;
     } else {
         emailErr.innerHTML = '';
-        emailField.setAttribute('style', 'background-color:none');
+        emailField.classList.remove('errcolor');
+        return true;
     };
 }
 
@@ -175,15 +187,15 @@ function validDivision(EO) {
 
     let divisionValue = Number(division.value);
     let divisionErr = division.nextElementSibling;
-    // if (divisionValue === 0) {
-    //     divisionErr.innerHTML = 'Вы не выбрали рубрику';
-    //     division.setAttribute('style', 'background-color: #f5bcbc');
+    
     if (divisionValue === 2) {
         divisionErr.innerHTML = 'Эта рубрика недоступна';
-        division.setAttribute('style', 'background-color: #f5bcbc');
+        division.classList.add('errcolor');
+        return false;
     } else {
         divisionErr.innerHTML = '';
-        division.setAttribute('style', 'background-color:none');
+        division.classList.remove('errcolor');
+        return true;
     };
 }
 
@@ -196,13 +208,14 @@ function validPayment(EO) {
     
     switch (paymentValue) {
         case (''):
-            paymentErr.innerHTML = 'Выберете вариант размещения';
-            break;
+            paymentErr.innerHTML = 'Вы не выбрали вариант размещения';
+            return false;
         case ('1'):
-            paymentErr.innerHTML = 'В данный момент эта опция отсутствует';
-            break;
+            paymentErr.innerHTML = 'В данный момент эта опция недоступна';
+            return false;
         default:
             paymentErr.innerHTML = '';
+            return true;
     }
 }
 
@@ -211,9 +224,11 @@ function validVotes(EO) {
 
     let votesErr = votes.nextElementSibling;
     if (!votes.checked) {
-        votesErr.innerHTML = 'Разрешите отзывы';
+        votesErr.innerHTML = 'Вы не разрешили отзывы';
+        return false;
     } else {
         votesErr.innerHTML = '';
+        return true;
     };
 }
 
@@ -224,15 +239,65 @@ function validDescription(EO) {
     let descriptionValue = description.value;
     let descriptionErr = description.nextElementSibling;
     if (!descriptionValue) {
-        descriptionErr.innerHTML = 'Введите описание сайта';
-        description.setAttribute('style', 'background-color: #f5bcbc');
+        descriptionErr.innerHTML = 'Вы не ввели описание сайта';
+        description.classList.add('errcolor');
+        return false;
     } else if (descriptionValue.length < 30) {
         descriptionErr.innerHTML = 'Описание должно быть более 30 символов';
-        description.setAttribute('style', 'background-color: #f5bcbc');
+        description.classList.add('errcolor');
+        return false;
     } else {
         descriptionErr.innerHTML = '';
-        description.setAttribute('style', 'background-color:none');
+        description.classList.remove('errcolor');
+        return true;
     };
 }
 
 
+function validFormAll(EO) {
+    EO = EO || window.event;
+    let value = true;
+    
+    try {
+        validDevelopers();
+        validSite();
+        validUrl();
+        validDate();
+        validVisitors();
+        validEmail();
+        validPayment();
+        validVotes();
+        validDescription();
+
+        value = validDevelopers(value);
+        value = validSite(value);
+        value = validUrl(value);
+        value = validDate(value);
+        value = validVisitors(value);
+        value = validEmail(value);
+        value = validPayment(value);
+        value = validVotes(value);
+        value = validDescription(value);
+
+        
+        if (value === false) {
+            EO.preventDefault();
+            focusToInput();
+        }
+        
+        
+    }
+    catch (ex) {
+        EO.preventDefault();
+    }
+}
+
+function focusToInput() {
+    
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].nextElementSibling.textContent != '') {
+            inputs[i].focus(); break;
+        } 
+        description.focus();
+    }
+}
