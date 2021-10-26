b453ddac33401e77a5bcc32546494b8447da54f9
A-new-ta <urievna86@gmail.com>
2021-10-27 00:46:40 +0300

    feat:continue solving

--- d1+/script.js
+++ d1+/script.js
@@ -1,77 +1,105 @@
 "use strict";
 
 const image = document.querySelector('.image');
+let inputData = {};
 
-document.addEventListener('load', main);
-
-
-
-
-function main() {
-
-    // добавить addEventListener
-
-    let currAction = EO.target.className;
-
-    let clickX = EO.pageX;
-    let clickY = EO.pageY;
-
-    // находим положение image при mousedown
+function getImageData(EO) {
     let pos = image.getBoundingClientRect();
-    let imgLeft = pos.left;
-    let imgTop = pos.top;
-    let imgWidth = pos.width;
-    let imgHeight = pos.height;
-
-    // находим сдвиг для перемещния image
-
-
-
-    function resize() {
-        
-        //прописываем условия для всех возможных кликов мыши
-
-        if (currAction === 'image') {
-
-        }
-
-        if (currAction === 'top__left') {
-            
-        }
-
-        if (currAction === 'top__middle') {
-            
-        }
-
-        if (currAction === 'top__right') {
-            
-        }
-
-        if (currAction === 'middle__right') {
-            
-        }
-
-        if (currAction === 'middle__right') {
-            
-        }
-
-        if (currAction === 'bottom__right') {
-            
-        }
-
-        if (currAction === 'bottom__middle') {
-            
-        }
-
-        if (currAction === 'bottom__left') {
-            
-        }
-
-        if (currAction === 'middle__left') {
-            
-        }
-
+    inputData = {
+        action: EO.target.className,
+        width: pos.width,
+        height: pos.height,
+        top: pos.top,
+        left: pos.left,
+        shiftX: EO.pageX - pos.left,
+        shiftY: EO.pageY - pos.top,
+        propWidth: pos.width / pos.height,
+        propHeight: (pos.width / pos.height) / 2
     }
+}
 
+document.addEventListener('mousedown', EO => {
+    EO.preventDefault();
+    getImageData(EO);
+    // let currAction = EO.target.className;
+    // let clickX = EO.pageX;
+    // let clickY = EO.pageY;
+
+    // // находим положение image при mousedown
+    // let pos = image.getBoundingClientRect();
+    // let imgLeft = pos.left;
+    // let imgTop = pos.top;
+    // let imgWidth = pos.width;
+    // let imgHeight = pos.height;
+
+    // //разница между кликом и расстоянием между image и окном браузера
+    // let shiftX = clickX - imgLeft;
+    // let shiftY = clickY - imgTop;
+
+    // //пропорции по ширине и высоте
+    // let propWidth = imgWidth / imgHeight;
+    // let propHeight = (imgWidth / imgHeight) / 2;
+
+    document.addEventListener('mousemove', mousemove);
+    document.addEventListener('mouseup', () => {
+        EO.preventDefault();
+        document.removeEventListener('mousemove', mousemove);
+    });
+
+});
+
+function mousemove(EO) {
+    // общие условия для клика
+    if (inputData.action) {
+
+        //прописываем условия для всех возможных кликов мыши
+        if (inputData.action === 'image') {
+
+        }
+
+        if (inputData.action === 'resizer top__left') {
+            
+        }
+
+        if (inputData.action === 'resizer top__middle') {
+            
+        }
+
+        if (inputData.action === 'resizer top__right') {
+            
+        }
+
+        if (inputData.action === 'resizer middle__right') {
+            
+        }
+
+        if (inputData.action === 'resizer middle__right') {
+            
+        }
+
+        if (inputData.action === 'resizer bottom__right') {
+            
+        }
+
+        if (inputData.action === 'resizer bottom__middle') {
+            
+        }
+
+        if (inputData.action === 'resizer bottom__left') {
+            
+        }
+
+        if (inputData.action === 'resizer middle__left') {
+            
+        }
+    } 
+    
 
 }
+
+
+
+
+
+
+
