60e0b3c01ac0496ada76b982d7f2e5998799e4d3
A-new-ta <urievna86@gmail.com>
2021-10-26 16:55:06 +0300

    feat: edit d1+

--- d1+/script.js
+++ d1+/script.js
@@ -1,1 +1,77 @@
 "use strict";
+
+const image = document.querySelector('.image');
+
+document.addEventListener('load', main);
+
+
+
+
+function main() {
+
+    // добавить addEventListener
+
+    let currAction = EO.target.className;
+
+    let clickX = EO.pageX;
+    let clickY = EO.pageY;
+
+    // находим положение image при mousedown
+    let pos = image.getBoundingClientRect();
+    let imgLeft = pos.left;
+    let imgTop = pos.top;
+    let imgWidth = pos.width;
+    let imgHeight = pos.height;
+
+    // находим сдвиг для перемещния image
+
+
+
+    function resize() {
+        
+        //прописываем условия для всех возможных кликов мыши
+
+        if (currAction === 'image') {
+
+        }
+
+        if (currAction === 'top__left') {
+            
+        }
+
+        if (currAction === 'top__middle') {
+            
+        }
+
+        if (currAction === 'top__right') {
+            
+        }
+
+        if (currAction === 'middle__right') {
+            
+        }
+
+        if (currAction === 'middle__right') {
+            
+        }
+
+        if (currAction === 'bottom__right') {
+            
+        }
+
+        if (currAction === 'bottom__middle') {
+            
+        }
+
+        if (currAction === 'bottom__left') {
+            
+        }
+
+        if (currAction === 'middle__left') {
+            
+        }
+
+    }
+
+
+}
