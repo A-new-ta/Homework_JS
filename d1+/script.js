9df16efcfac42d3a327e363368a8442883d05688
A-new-ta <urievna86@gmail.com>
2021-10-28 01:35:07 +0300

    feat: complete task d1+

--- d1+/script.js
+++ d1+/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const image = document.querySelector('.image');
+let image = document.querySelector('.image');
 let inputData = {};
 
 
@@ -15,32 +15,14 @@ function getImageData(EO) {
         shiftX: EO.pageX - pos.left,
         shiftY: EO.pageY - pos.top,
         propWidth: pos.width / pos.height,
-        propHeight: (pos.width / pos.height) / 2
+        propHeight: pos.height / pos.width,
     }
 }
 
 document.addEventListener('mousedown', EO => {
     EO.preventDefault();
     getImageData(EO);
-    // let currAction = EO.target.className;
-    // let clickX = EO.pageX;
-    // let clickY = EO.pageY;
-
-    // // находим положение image при mousedown
-    // let pos = image.getBoundingClientRect();
-    // let imgLeft = pos.left;
-    // let imgTop = pos.top;
-    // let imgWidth = pos.width;
-    // let imgHeight = pos.height;
-
-    // //разница между кликом и расстоянием между image и окном браузера
-    // let shiftX = clickX - imgLeft;
-    // let shiftY = clickY - imgTop;
-
-    // //пропорции по ширине и высоте
-    // let propWidth = imgWidth / imgHeight;
-    // let propHeight = (imgWidth / imgHeight) / 2;
-
+    
     document.addEventListener('mousemove', mousemove);
     document.addEventListener('mouseup', () => {
         EO.preventDefault();
@@ -51,12 +33,13 @@ document.addEventListener('mousedown', EO => {
 
 function mousemove(EO) {
 
-    // общие условия для клика
-    if (inputData.action) {
-        // let mouseLeft = EO.pageX - inputData.left;
-        // let mouseTop = EO.pageY - inputData.top;
-        // let mouseRight = EO.pageX - (inputData.left + inputData.width);
-        // let mouseBottom = EO.pageY - (inputData.top + inputData.height);
+    // смещение мыши по x и y при движении
+    if (inputData.action !== '') {
+        let mouseLeft = EO.pageX - inputData.left;
+        let mouseTop = EO.pageY - inputData.top;
+        let mouseRight = EO.pageX - (inputData.left + inputData.width);
+        let mouseBottom = EO.pageY - (inputData.top + inputData.height);
+        
         //прописываем условия для всех возможных кликов мыши
         if (inputData.action === 'image') {
             image.style.top = EO.pageY - inputData.shiftY + 'px';
@@ -65,59 +48,97 @@ function mousemove(EO) {
 
         if (inputData.action === 'top__left') {
            
+            image.style.height = inputData.height - mouseTop + 'px';
             image.style.width = image.offsetHeight * inputData.propWidth + 'px';
-            image.style.height = inputData.height - (EO.pageY - inputData.top) + 'px';
-            
-            image.style.left = (inputData.left + inputData.width) - image.offsetWidth + 'px';
             image.style.top = (inputData.top + inputData.height) - image.offsetHeight + 'px';
-        
+            image.style.left = (inputData.left + inputData.width) - image.offsetWidth + 'px';
+            
             if (EO.pageY >= inputData.top + inputData.height && EO.pageX >= inputData.left + inputData.width) {
-                image.style.height = EO.pageY - (inputData.top + inputData.height) + 'px';
-                image.style.top = inputData.top + inputData.height + 'px';
+                image.style.height = mouseBottom + 'px';
                 image.style.width = image.offsetHeight * inputData.propWidth + 'px';
+                image.style.top = inputData.top + inputData.height + 'px';
                 image.style.left = inputData.left + inputData.width + 'px';
             }
         }
 
         if (inputData.action === 'top__middle') {
-            image.style.top = EO.pageY  + 'px';
-            image.style.height = inputData.height - (EO.pageY - inputData.top) + 'px';
+            // image.style.top = EO.pageY + 'px';
+            image.style.top = inputData.top + mouseTop + 'px';
+            image.style.height = inputData.height - mouseTop + 'px';
+            
             if (EO.pageY >= inputData.top + inputData.height) {
                 image.style.top = inputData.top + inputData.height + 'px';
-                image.style.height = (EO.pageY - inputData.top) - inputData.height + 'px';
+                image.style.height = mouseTop - inputData.height + 'px';
             }
         }
 
         if (inputData.action === 'top__right') {
-            
+            image.style.height = inputData.height - mouseTop + 'px';
+            image.style.width = image.offsetHeight / inputData.propHeight + 'px';
+            image.style.top = inputData.top + mouseTop + 'px';
+
+            if (EO.pageY >= inputData.top + inputData.height && EO.pageX <= inputData.left) {
+                image.style.width = - mouseRight - inputData.width + 'px';
+                image.style.height = image.offsetWidth / inputData.propWidth + 'px';
+                image.style.left = mouseRight + inputData.width + inputData.left + 'px';
+                image.style.top = inputData.top + inputData.height + 'px';
+            }
         }
 
         if (inputData.action === 'middle__right') {
-            
+            image.style.width = mouseRight + inputData.width + 'px';
+
+            if (EO.pageX <= inputData.left) {
+                image.style.width = - mouseRight - inputData.width + 'px';
+                image.style.left = mouseRight + inputData.width + inputData.left + 'px';
+            }
         }
 
-        if (inputData.action === 'middle__right') {
-            
-        }
-
+        
         if (inputData.action === 'bottom__right') {
+            image.style.width = mouseLeft - inputData.propWidth + 'px';
+            image.style.height = image.offsetWidth / inputData.propWidth + 'px';
             
+            if (EO.pageY <= inputData.top && EO.pageX <= inputData.left) {
+                image.style.width = - mouseRight - inputData.width + 'px';
+                image.style.height = image.offsetWidth / inputData.propWidth + 'px';
+                image.style.left = inputData.left - image.offsetWidth + 'px';
+                image.style.top = inputData.top - image.offsetHeight + 'px';
+            }
         }
 
         if (inputData.action === 'bottom__middle') {
-            
+            image.style.height = mouseBottom + inputData.height + 'px';
+
+            if (EO.pageY <= inputData.top) {
+                image.style.top = mouseBottom + inputData.height + inputData.top + 'px';
+                image.style.height = - mouseBottom - inputData.height + 'px';
+            }
         }
 
         if (inputData.action === 'bottom__left') {
-            
+            image.style.width = inputData.width - mouseLeft + 'px';
+            image.style.left = inputData.left + mouseLeft + 'px';
+            image.style.height = image.offsetWidth / inputData.propWidth + 'px';
+
+            if (EO.pageX >= inputData.left + inputData.width && EO.pageY <= inputData.top) {
+                image.style.height = -mouseBottom - inputData.height + 'px';
+                image.style.top = mouseBottom + inputData.height + inputData.top + 'px';
+                image.style.width = image.offsetHeight * inputData.propWidth + 'px';
+                image.style.left = inputData.left + inputData.width + 'px';
+            }
         }
 
         if (inputData.action === 'middle__left') {
-            
+            image.style.width = inputData.width - mouseLeft + 'px';
+            image.style.left = inputData.left + mouseLeft + 'px';
+
+            if (EO.pageX >= inputData.left + inputData.width) {
+                image.style.width = mouseLeft - inputData.width + 'px';
+                image.style.left = inputData.left + inputData.width + 'px';
+            }
         }
     } 
-    
-
 }
 
 
