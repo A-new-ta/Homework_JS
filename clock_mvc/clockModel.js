430311cd5a69912b356ae422d33da9a05b2c86d8
A-new-ta <urievna86@gmail.com>
2021-11-27 23:07:34 +0300

    fix:correct dyn_form_ajax task

---
+++ clock_mvc/clockModel.js
@@ -0,0 +1,49 @@
+"use strict";
+
+class Clock {
+    constructor(name, time) {
+      this.name = name;
+      this.time = time;
+      this.view = null;
+  
+      this.d = new Date();
+      this.youTimeZone = this.d.getTimezoneOffset();
+  
+      this.timer = null;
+      }
+  
+    timeConvertor() {
+      let minskTime = this.youTimeZone * 60 * 1000;
+      let hours = this.time * 60 * 60 * 1000;
+      let date = new Date();
+      let time = date.getTime() + hours;
+      return new Date(time + +minskTime);
+      }
+  
+    start(view) {
+      this.view = view;
+      this.view.clockGo(this.timeConvertor());
+      this.updateView();
+      this.timerStart();
+    }
+  
+    updateView() {
+      if(this.view) {
+        this.view.update(this.timeConvertor());
+      }
+    }
+  
+    timerStop() {
+      window.clearInterval(this.timer)
+    }
+  
+    timerStart() {
+      let that = this;
+      this.timer = window.setInterval(
+        function(){
+          this.d = that.timeConvertor();
+          that.updateView();
+        }
+        , 500);
+    }
+  }
---
+++ clock_mvc/index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+window.addEventListener('load', function () {
+    let container = document.querySelector('.container');
+  
+    let clock = new Clock('Нью-Йорк', '-5');
+    let clockDom = new ClockViewDOM();
+    let controller = new ClockControllerButtons();
+  
+    clock.start(clockDom);
+    clockDom.start(clock, container, 1);
+    controller.start(clock, 1);
+  
+  
+    let clock2 = new Clock('Лондон', '1');
+    let clockDom2 = new ClockViewDOM();
+    let controller2 = new ClockControllerButtons();
+  
+    clock2.start(clockDom2);
+    clockDom2.start(clock2, container, 2);
+    controller2.start(clock2, 2);
+  
+    let clockSvg = new Clock('Берлин', '3');
+    let clockDomSvg = new ClockViewSVG();
+    let controllerSvg = new ClockControllerButtons();
+  
+    clockSvg.start(clockDomSvg);
+    clockDomSvg.start(clockSvg, container, 3);
+    controllerSvg.start(clockSvg, 3);
+    // clockDomSvg.render();
+  
+    let clockSvg2 = new Clock('Токио', '7');
+    let clockDomSvg2 = new ClockViewSVG();
+    let controllerSvg2 = new ClockControllerButtons();
+  
+    clockSvg2.start(clockDomSvg2);
+    clockDomSvg2.start(clockSvg2, container, 4);
+    controllerSvg2.start(clockSvg2, 4);
+  
+  });
